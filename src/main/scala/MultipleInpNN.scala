23fa1eedd9d57edb17bc40f7b6c2b1436a9a1b7e
Roxana Tesileanu <roxana.te@web.de>
2017-05-31 09:18:40 +0300

    Multiple Inputs NN prediction

--- src/main/scala/MultipleInpNN.scala
+++ src/main/scala/MultipleInpNN.scala
@@ -5,4 +5,9 @@ val myPairProd = for (p <- myPairs) yield {p._1 * p._2}
 val dotProduct = myPairProd.sum
 dotProduct
 }
+
+val myW = Array(0.1, 0.2, 0)
+val myI = Array(8.5, 0.65, 1.2)
+val myPred = neural_network(myW, myI)
+
 }
