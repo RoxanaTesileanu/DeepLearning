537f7395c6e976afb36d307f12b3d4a66f8c2250
Roxana Tesileanu <roxana.te@web.de>
2017-05-31 10:37:06 +0300

    task: write elementwise operations for two vectors

--- src/main/scala/MultipleInpNN.scala
+++ src/main/scala/MultipleInpNN.scala
@@ -10,4 +10,15 @@ val myW = Array(0.1, 0.2, 0)
 val myI = Array(8.5, 0.65, 1.2)
 val myPred = neural_network(myW, myI)
 
+def arrayOp(myWeights : Array[Double], myInputs : Array[Double],
+f: (Double, Double) => Double) = {
+val myPairs = myWeights zip myInputs
+val myOp = for (p <- myPairs) yield {f(p._1, p._2)}
+myOp
+}
+
+val elementwiseAddition = arrayOp(myW, myI, _ + _)
+
+val elementwiseMultiplication = arrayOp(myW, myI, _ * _)
+
 }
