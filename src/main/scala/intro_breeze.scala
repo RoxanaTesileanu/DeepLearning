a09f2b08a38970764129fbc82530f45f37c05ac0
Roxana Tesileanu <roxana.te@web.de>
2017-06-06 13:20:51 +0300

    adding the scala-glm library to build.sbt

--- src/main/scala/intro_breeze.scala
+++ src/main/scala/intro_breeze.scala
@@ -2,6 +2,7 @@
 */
 
 import breeze.stats.distributions._
+import breeze.linalg._
 
 object IntroBreeze {
 val poi = Poisson(3.0)
@@ -15,6 +16,13 @@ val y = gau.sample(20)
 
 val logPdfYs= y map { gau.logPdf(_)}
 
+
+val v= DenseVector(y.toArray) // create a Vector out of the sample
+val exBitVect= v >:> 0.0 // returns the indexes of the elements for which the Boolean expression is true
+
+val exBooleanResult = (v >:> 0.0).toArray //returns the Boolean result for each element of the array
+
+val m = new DenseMatrix(5,4,linspace(1.0,20.0, 20).toArray) // the elements are arranged column-by-column
 
 
 }
